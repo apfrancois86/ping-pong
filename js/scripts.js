fef248eb0d9552c320bf16bc04e8b0cbf110a235
Alex Francois <apfrancois86@gmail.com>
2016-10-28 08:55:03 -0700

    add logic for spec 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
+//business logic
+var pingPong = function(number);
+  var outputArray= [];
+  for (index = 1; index <= number; index++) {
+    if (index % 3 === 0) {
+      outputArray.push("ping");
+    } else if (index) {
+
+    }
+  }
+
+
+
+
+
+//front end logic
 $(document).ready(function(){
   $("#pingpong").submit(function(event)(
     event.preventDefault();
     var input = parseInt($("input#number").val());
     var results = pingPong(input);
+    $("#output").empty();
+
+    results.forEach(result);
+    $("#output").append("<li>" + result + "</li>");
+
   ));
 });
