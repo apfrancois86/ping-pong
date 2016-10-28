7b77e34a433583f4de875965dea60bc0d601eefd
Alex Francois <apfrancois86@gmail.com>
2016-10-28 09:52:40 -0700

    create if statement to make user enter a number greater than 0 otherwise user will get alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 //business logic
 var pingPong = function(number) {
-  var outputArray= [];
-  for (index = 1; index <= number; index++) {
-    if (index % 15 === 0) {
-      outputArray.push("ping-pong");
-      console.log(outputArray);
-    } else if (index % 5 === 0) {
-      outputArray.push("pong");
-    } else if (index % 3 === 0) {
-      outputArray.push("ping");
-    } else {
-      outputArray.push(index);
-    }
-  } return outputArray;
+  if (number>0) {
+    var outputArray= [];
+      for (index = 1; index <= number; index++) {
+        if (index % 15 === 0) {
+          outputArray.push("ping-pong");
+          console.log(outputArray);
+        } else if (index % 5 === 0) {
+          outputArray.push("pong");
+        } else if (index % 3 === 0) {
+          outputArray.push("ping");
+        } else {
+          outputArray.push(index);
+        }
+      } return outputArray;
+  } else {
+    return alert ("Enter a number greater than 0");
+  }
 }
-
-
-
 
 
 //front end logic
 $(document).ready(function(){
-  $("#pingpong").submit(function(event){
+  $("#ping-pong").submit(function(event){
     event.preventDefault();
     var input = parseInt($("input#number").val());
     console.log(input);
